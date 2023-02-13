ed29c754785f1216d008438229784d698b9d3faa
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-02-13 16:27:29 +0100

    carico eserecizio dei numeri pari fino a 20

---
+++ numeriparifinoa20.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+int main()
+{
+    int n;
+    n = 20;
+
+    for (int i = 0; i <= n; i += 2)
+    {
+        cout << i << " \n";
+    }
+    return 0;
+}
