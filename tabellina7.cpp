ac51781cf9106c247c4f569bd542068642ee6627
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-02-13 15:49:24 +0100

    salvo l'esercizio sulla tabellina del 7

---
+++ tabellina7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+int main () 
+{
+ int n;
+ n = 10;
+ int a;
+ a = 7;
+
+ for (int i = 1; i <= n; i++)
+{ cout << "a * " << i << " = " << a * i << " \n";
+}
+return 0;
+}
