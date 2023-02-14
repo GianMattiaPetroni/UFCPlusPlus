0ab38498223489b2b1ebbdf389fe8870292f566e
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-02-14 11:14:30 +0100

    carico esercizi

---
+++ sommaprimicentonumeri.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int somma = 0;
+    for (int i = 1; i <= 100; i++;)
+    {
+        somma += 1;
+     }
+    cout << "La somma dei primi 100 numeri interi è: " << somma << "/n";
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int totale(int a)
+{
+
+    int i = 0;
+    int somma = 0 while (i <= a)
+    {
+        somma += i * i;
+        i++;
+    }
+    return somma;
+}
+int main()
+{
+    int somma = totale(10);
+
+    cout << "la somma dei primi 10 numeri quadrati e': " << somma << endl;
+    return 0;
+}
